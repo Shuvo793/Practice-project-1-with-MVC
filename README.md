5a2a22f0097ccb08cc334ead83d54446237999bf
Shuvo793 <cse.engr.shuvo@gmail.com>
2019-05-01 01:51:20 +0600

    README.md file edit

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Ecommerce system development in PHP 7 with MVC pettern
  
  *Installation Instruction*
   - clone the repo
-  - run terminal `composer install`
-  - run terminal `npm install`
+  - composer install in your computer and run terminal `composer install` in this project file
+  - node.js install in your computer and run terminal `npm install` in this project
   - creat a database and export the `.sql` file into database
   - setting your server and run this project
   
--- partials/FrontEnd/__footer.php
+++ partials/FrontEnd/__footer.php
@@ -5,7 +5,7 @@
                 </a></button>
         </p>
         <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-        <p> All rights reserved RI suvo &copy; <?php echo date('Y')?> <a href="https://github.com/Shuvo793/Village_Bazar_Project_MVC" style="text-decoration: none;">Visit <i class="fab fa-github"></i> page</a> or read our <a href="/docs/4.3/getting-started/introduction/">getting started guide</a>.</p>
+        <p> All rights reserved RI suvo &copy; <?php echo date('Y')?> <a href="https://github.com/Shuvo793" style="text-decoration: none;" target="_blank">Visit <i class="fab fa-github"></i> github profile</a> or read our <a href="https://github.com/Shuvo793/Village_Bazar_Project_MVC" target="_blank">getting started guide</a>.</p>
     </div>
 </footer>
 <!--script start-->
